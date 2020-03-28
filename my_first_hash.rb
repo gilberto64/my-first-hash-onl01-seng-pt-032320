c80340e09df53fa50e5962c901910fa2701aa145
Gilberto Ocampo <ocampo@gate.net>
2020-03-28 21:42:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,12 +27,13 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  
+
+ 
   shipping_manifest
   
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest = {"gun powder" => 4, "muskets" => 2} 
+
 
   # add 4 gun powder to the shipping_manifest hash below
   # return the shipping_manifest hash below
